91d477ba93fb36a9babc8f4cc4fa35549c77ce25
Michael Kreimer <michaelkreimer@gmail.com>
2016-05-07 22:43:00 +0300

    garage skeleton

--- B16 Ex03 MichaelKreimer 305597478 IdoPerry 036928646/Ex3.GarageLogic/Garage.cs
+++ B16 Ex03 MichaelKreimer 305597478 IdoPerry 036928646/Ex3.GarageLogic/Garage.cs
@@ -6,6 +6,46 @@ namespace Ex3.GarageLogic
 {
     class Garage
     {
-       
+        public enum eVehicleStatus
+        {
+
+        }
+        private Dictionary<string, Vehicle> m_Vehicles = new Dictionary<string, Vehicle>();
+
+        public void InsertVehicle(Vehicle io_Vehicle)
+        {
+            //1 - TODO
+        }
+        public void DisplayVehiclesById()
+        {
+            //2 - TODO 
+            // change signature
+        }
+        public void ChangeVehicleStatus(Vehicle io_Vehicle)
+        {
+            //3 - TODO
+            // change signature
+        }
+        public void FillTires(string i_VehicleID, )
+        {
+            //4 - TODO
+            // change signature
+        }
+        public void FillGasVehicle()
+        {
+            //5 - TODO
+            // change signature
+        }
+        public void ChargeElectricVehicle()
+        {
+            //6 - TODO
+            // change signature
+        }
+        public void DisplayVehicleData()
+        {
+            //7 - TODO
+            // change signature
+        }
+
     }
 }
